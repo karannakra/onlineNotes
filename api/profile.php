f88608f99a6c97b6e73f547ac434ffd50430a94f
Karan Nakra <karannakra@Karan-work-MB-Air.local>
2023-12-08 15:26:07 +0530

    renaming

--- api/profile.php
+++ api/profile.php
@@ -216,7 +216,7 @@ if(!isset($_SESSION['user_id'])){
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <script src="index.js"></script>
-<script src="profile.js"></script>
-<script src="loadnotes.js"></script>
+<script src="profile-1.js"></script>
+<script src="loadnotes-1.js"></script>
 </body>
 </html>
